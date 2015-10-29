2c1dca720f5f39d2f775e59a67137e0cead5911c
kai <kai@kunfoo.org>
2015-10-29 13:47:41 +0100

    added option for padding character, getopt

--- permutation_cipher.c
+++ permutation_cipher.c
@@ -1,9 +1,12 @@
+#define _XOPEN_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <time.h>
 #include <ctype.h>
+#include <unistd.h>
 
 int check_key(char *key, size_t key_len) {
     char key_digits[key_len];
@@ -39,42 +42,85 @@ char * compute_inverse_key(char *key, size_t key_len) {
     return inverse_key;
 }
 
+void print_usage(char *s) {
+    printf("usage: %s [-p P] key plaintext\n", s);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("usage: %s key text\n", argv[0]);
-        exit(EXIT_FAILURE);
+    int opt;
+    char *padding = NULL;
+
+    while ((opt = getopt(argc, argv, "p:")) != -1) {
+        switch (opt) {
+            case 'p':
+                padding = optarg;
+                if (strlen(padding) != 1) {
+                    printf("error: padding must be a single character\n");
+                    print_usage(argv[0]);
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case '?':
+                if (optopt == 'p') {
+                    printf("error: option -%c requires an argument.\n", optopt);
+                    print_usage(argv[0]);
+                }
+                else {
+                    printf("error: unknown option `-%c'.\n", optopt);
+                    print_usage(argv[0]);
+                }
+                exit(EXIT_FAILURE);
+            default:
+                printf("unknown error\n");
+                exit(EXIT_FAILURE);
+        }
     }
 
-    char *key = argv[1];
+    if ((argc - optind) != 2) {
+        print_usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    char *key = argv[optind];
+    char *plaintext = argv[optind + 1];
+
+    printf("padding: %s\n", padding);
+    printf("key: %s\n", key);
+    printf("plaintext: %s\n", plaintext);
+
     size_t key_len = strlen(key);
     if (key_len > 10) {
         printf("error: key must not be longer than 10 digits!\n");
         exit(EXIT_FAILURE);
     }
 
-    if (0 != check_key(argv[1], key_len)) {
+    if (0 != check_key(key, key_len)) {
         printf("error: invalid key\n");
         exit(EXIT_FAILURE);
     }
 
-    char *plaintext = argv[2];
     size_t text_len = strlen(plaintext);
     size_t pad_len = (size_t) (text_len % key_len);
     if (pad_len != 0) {
         pad_len = key_len - pad_len;
-        plaintext = malloc(text_len + pad_len + 1);
-        if (NULL == plaintext) {
+        char *tmp = malloc(text_len + pad_len + 1);
+        if (NULL == tmp) {
             perror(NULL);
             exit(EXIT_FAILURE);
         }
-        memcpy(plaintext, argv[2], text_len);
+        memcpy(tmp, plaintext, text_len);
         srand(time(NULL));
         for (size_t i = 0; i < pad_len; ++i) {
             // pad with any non-whitespace printable ascii char
-            plaintext[text_len + i] = '!' + (rand() % 94);
+            if (padding == NULL) {
+                tmp[text_len + i] = '!' + (rand() % 94);
+            }
+            else {
+                tmp[text_len + i] = padding[0];
+            }
         }
-        plaintext[text_len + pad_len] = 0;
+        tmp[text_len + pad_len] = 0;
         text_len += pad_len;
+        plaintext = tmp;
     }
 
     char ciphertext[text_len + 1];
@@ -95,13 +141,13 @@ int main(int argc, char *argv[]) {
     }
 
     if (pad_len) {
-        printf("plaintext was padded with \"");
+        printf("plaintext was padded with \'");
         for (size_t idx = text_len - pad_len; idx < text_len; ++idx) {
             printf("%c", plaintext[idx]);
         }
-        printf("\"\n");
+        printf("\'\n");
     }
-    printf("ciphertext is \"%s\"\n", ciphertext);
+    printf("ciphertext is %s\n", ciphertext);
 
     free(inverse_key);
     if (plaintext != argv[2]) {
