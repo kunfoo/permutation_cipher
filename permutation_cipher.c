2efcdb57270ad29b1c1470c7fd801b2e572d5f48
kai <kai@kunfoo.org>
2015-10-30 12:29:47 +0100

    computation of inverse key can now be turned on/off by switch -i

--- permutation_cipher.c
+++ permutation_cipher.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE
+#define _XOPEN_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -47,11 +47,18 @@ void print_usage(char *s) {
 }
 
 int main(int argc, char *argv[]) {
+    char do_compute_inverse_key = 0;
     int opt;
     char *padding = NULL;
 
-    while ((opt = getopt(argc, argv, "p:")) != -1) {
+    while ((opt = getopt(argc, argv, "ip:")) != -1) {
         switch (opt) {
+            case 'h':
+                print_usage(argv[0]);
+                exit(EXIT_SUCCESS);
+            case 'i':
+                do_compute_inverse_key = 1;
+                break;
             case 'p':
                 padding = optarg;
                 if (strlen(padding) != 1) {
@@ -76,16 +83,21 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if ((argc - optind) != 2) {
-        print_usage(argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    char *key = argv[optind];
-    char *plaintext = argv[optind + 1];
+    char *key = NULL;
+    char *plaintext = NULL;
 
-    printf("padding: %s\n", padding);
-    printf("key: %s\n", key);
-    printf("plaintext: %s\n", plaintext);
+    switch (argc - optind) {
+        case 1:
+            key = argv[optind];
+            break;
+        case 2:
+            key = argv[optind];
+            plaintext = argv[optind + 1];
+            break;
+        default:
+            print_usage(argv[0]);
+            exit(EXIT_FAILURE);
+    }
 
     size_t key_len = strlen(key);
     if (key_len > 10) {
@@ -98,61 +110,62 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    size_t text_len = strlen(plaintext);
-    size_t pad_len = (size_t) (text_len % key_len);
-    if (pad_len != 0) {
-        pad_len = key_len - pad_len;
-        char *tmp = malloc(text_len + pad_len + 1);
-        if (NULL == tmp) {
-            perror(NULL);
-            exit(EXIT_FAILURE);
+    if (do_compute_inverse_key == 1) {
+        char *inverse_key = compute_inverse_key(key, key_len);
+        if (NULL == inverse_key) {
+            printf("error computing inverse key\n");
         }
-        memcpy(tmp, plaintext, text_len);
-        srand(time(NULL));
-        for (size_t i = 0; i < pad_len; ++i) {
-            // pad with any non-whitespace printable ascii char
-            if (padding == NULL) {
-                tmp[text_len + i] = '!' + (rand() % 94);
+        else {
+            printf("inverse key is %s\n", inverse_key);
+            free(inverse_key);
+        }
+    }
+
+    if (plaintext) {
+        size_t text_len = strlen(plaintext);
+        size_t pad_len = (size_t) (text_len % key_len);
+        if (pad_len != 0) {
+            pad_len = key_len - pad_len;
+            char *tmp = malloc(text_len + pad_len + 1);
+            if (NULL == tmp) {
+                perror(NULL);
+                exit(EXIT_FAILURE);
             }
-            else {
-                tmp[text_len + i] = padding[0];
+            memcpy(tmp, plaintext, text_len);
+            srand(time(NULL));
+            for (size_t i = 0; i < pad_len; ++i) {
+                // pad with any non-whitespace printable ascii char
+                if (padding == NULL) {
+                    tmp[text_len + i] = '!' + (rand() % 94);
+                }
+                else {
+                    tmp[text_len + i] = padding[0];
+                }
             }
+            tmp[text_len + pad_len] = 0;
+            text_len += pad_len;
+            plaintext = tmp;
         }
-        tmp[text_len + pad_len] = 0;
-        text_len += pad_len;
-        plaintext = tmp;
-    }
 
-    char ciphertext[text_len + 1];
-    ciphertext[text_len] = 0;
+        char ciphertext[text_len + 1];
+        ciphertext[text_len] = 0;
 
-    for (size_t i = 0; i < text_len; ++i) {
-        size_t key_idx = i % key_len;
-        size_t pt_idx = (i / key_len) * key_len + (size_t) (key[key_idx] - '0');
-        ciphertext[i] = plaintext[pt_idx];
-    }
-
-    char *inverse_key = compute_inverse_key(key, key_len);
-    if (NULL == inverse_key) {
-        printf("error computing inverse key\n");
-    }
-    else {
-        printf("inverse key is %s\n", inverse_key);
-    }
-
-    if (pad_len) {
-        printf("plaintext was padded with \'");
-        for (size_t idx = text_len - pad_len; idx < text_len; ++idx) {
-            printf("%c", plaintext[idx]);
+        for (size_t i = 0; i < text_len; ++i) {
+            size_t key_idx = i % key_len;
+            size_t pt_idx = (i / key_len) * key_len + (size_t) (key[key_idx] - '0');
+            ciphertext[i] = plaintext[pt_idx];
         }
-        printf("\'\n");
-    }
-    printf("ciphertext is %s\n", ciphertext);
 
-    free(inverse_key);
-    if (plaintext != argv[2]) {
-        free(plaintext);
+        if (pad_len) {
+            printf("plaintext was padded with \'");
+            for (size_t idx = text_len - pad_len; idx < text_len; ++idx) {
+                printf("%c", plaintext[idx]);
+            }
+            printf("\'\n");
+            free(plaintext);
+        }
+        printf("ciphertext is %s\n", ciphertext);
     }
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }
